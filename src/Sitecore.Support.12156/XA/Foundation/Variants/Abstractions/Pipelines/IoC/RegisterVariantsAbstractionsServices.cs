f0a219bea14eafb94acadd871ae4eb888f27e2f0
AntonKechashin <atk@sitecore.net>
2018-03-27 17:27:28 +0300

    Replaced the RegisterVariantsAbstractionsServices processor

---
+++ src/Sitecore.Support.12156/XA/Foundation/Variants/Abstractions/Pipelines/IoC/RegisterVariantsAbstractionsServices.cs
@@ -0,0 +1,20 @@
+﻿namespace Sitecore.Support.XA.Foundation.Variants.Abstractions.Pipelines.IoC
+{
+  using Microsoft.Extensions.DependencyInjection;
+  using Sitecore.XA.Foundation.IOC.Pipelines.IOC;
+  using Sitecore.XA.Foundation.Variants.Abstractions.Renderers;
+  using Sitecore.XA.Foundation.Variants.Abstractions.Services;
+
+  public class RegisterVariantsAbstractionsServices : IocProcessor
+  {
+    public override void Process(IocArgs args)
+    {
+      args.ServiceCollection.AddTransient<IVariantFieldParser, VariantFieldParser>();
+      args.ServiceCollection.AddSingleton<ITemplateRenderer, Sitecore.Support.XA.Foundation.Variants.Abstractions.Services.TemplateRenderer>();
+      args.ServiceCollection.AddTransient<IVariantRenderer, VariantRenderer>();
+      args.ServiceCollection.AddTransient<IAvailableRenderingVariantService, AvailableRenderingVariantService>();
+      args.ServiceCollection.AddTransient<IVariantRenderingService, VariantRenderingService>();
+      args.ServiceCollection.AddTransient<IRulesService, RulesService>();
+    }
+  }
+}
